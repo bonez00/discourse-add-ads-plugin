30089fc81c06f4be7aa65eb05762b17783d8efdd
bonez00 <47571602+bonez00@users.noreply.github.com>
2019-02-12 18:25:55 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# discourse-add-ads-plugin
+Add media-clic ads for some places in discourse
---
+++ plugin.rb
@@ -0,0 +1,4 @@
+# name: add-ads-plugin
+# about: A super simple plugin to add media-clic adds before suggested-topics, topic title, topic list first, and end of the website
+# version: 0.0.1
+# authors: Raspbian France (https://raspbian-france.fr)
